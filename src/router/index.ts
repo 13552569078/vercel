52f30315fa33721ab3eaffc400bfa8b8dc94128e
zhanghongjie <zhanghongjie@cestc.cn>
2024-09-19 12:22:37 +0800

    feat: 布局

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'Diff',
-      component: () => import('@/views/diff/index.vue'),
+      component: () => import('@/views/diff-index/index.vue'),
       meta: {
         title: 'diff',
       },
