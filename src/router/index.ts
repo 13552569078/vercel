019e5f26a0b0416bdcd2a59344e680465cf7fbb5
zhanghongjie <zhanghongjie@cestc.cn>
2024-09-19 14:44:33 +0800

    feat: 绘制

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,16 +3,16 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHashHistory(`/${import.meta.env.PROD_PREFIX}`),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Diff',
+    //   component: () => import('@/views/diff-index/index.vue'),
+    //   meta: {
+    //     title: 'diff',
+    //   },
+    // },
     {
       path: '/',
-      name: 'Diff',
-      component: () => import('@/views/diff-index/index.vue'),
-      meta: {
-        title: 'diff',
-      },
-    },
-    {
-      path: '/draw',
       name: 'Draw',
       component: () => import("@/views/HomeView.vue"),
       meta: {
